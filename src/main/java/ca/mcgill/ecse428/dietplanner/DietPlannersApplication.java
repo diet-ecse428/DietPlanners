e950c8c0d35725db8f8e7cc9d0441a2722d2ea8b
Noam Suissa <noam.suissa01@gmail.com>
2019-01-31 17:55:35 -0500

    spring init

---
+++ src/main/java/ca/mcgill/ecse428/dietplanner/DietPlannersApplication.java
@@ -0,0 +1,14 @@
+package ca.mcgill.ecse428.dietplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietPlannersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietPlannersApplication.class, args);
+	}
+
+}
+
