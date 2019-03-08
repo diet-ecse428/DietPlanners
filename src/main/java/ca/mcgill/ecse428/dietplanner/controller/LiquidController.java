df69a120bdeec23d8a7cab78873412a93b71d65b
Noam Suissa <noam.suissa01@gmail.com>
2019-03-07 19:18:35 -0500

    liquid create/get complete

--- src/main/java/ca/mcgill/ecse428/dietplanner/controller/LiquidController.java
+++ src/main/java/ca/mcgill/ecse428/dietplanner/controller/LiquidController.java
@@ -1,5 +1,49 @@
 package ca.mcgill.ecse428.dietplanner.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.mcgill.ecse428.dietplanner.dto.EntryDTO;
+import ca.mcgill.ecse428.dietplanner.dto.LiquidDTO;
+import ca.mcgill.ecse428.dietplanner.model.Liquid;
+import ca.mcgill.ecse428.dietplanner.repository.LiquidRepository;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/food")
 public class LiquidController {
 
+	@Autowired
+	private LiquidRepository repository;
+	
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	@ResponseBody
+	public LiquidDTO createLiquid(@RequestParam("entryid") int entry_id, @RequestParam("calories") int calories, @RequestParam("volume") double volume) {
+		Liquid result = repository.createLiquid(entry_id, calories, volume);
+		if(result!=null) {
+			LiquidDTO liquid = new LiquidDTO(result.getCalories(), result.getVolume(), result.getId(), result.getEntryId());
+			return liquid;
+		}else {
+			return null;
+		}
+	}
+	
+	@GetMapping("/get/{liquidId}")
+	public LiquidDTO getLiquid(@PathVariable("liquidId") int liquidId) {
+		Liquid result = repository.getLiquid(liquidId);
+		if(result!=null) {
+			LiquidDTO liquid = new LiquidDTO(result.getCalories(), result.getVolume(), result.getId(), result.getEntryId());
+			return liquid;
+		}else {
+			return null;
+		}
+	}
+	
 }
--- src/main/java/ca/mcgill/ecse428/dietplanner/repository/LiquidRepository.java
+++ src/main/java/ca/mcgill/ecse428/dietplanner/repository/LiquidRepository.java
@@ -1,5 +1,38 @@
 package ca.mcgill.ecse428.dietplanner.repository;
 
-public class LiquidRepository {
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse428.dietplanner.model.Entry;
+import ca.mcgill.ecse428.dietplanner.model.Liquid;
+
+@Repository
+public class LiquidRepository {
+	
+	@PersistenceContext
+	public EntityManager em;
+
+	@Transactional
+	public Liquid createLiquid(int entry_id, int calories, double volume) {
+		Entry entry = em.find(Entry.class, entry_id);
+		if(entry==null || calories<0 || volume<0) {
+			return null;
+		}
+		Liquid liquid = new Liquid();
+		liquid.setCalories(calories);
+		liquid.setEntryId(entry_id);
+		liquid.setVolume(volume);
+		
+		em.persist(liquid);
+		return liquid;
+	}
+
+	@Transactional
+	public Liquid getLiquid(int liquidId) {
+		Liquid liquid = em.find(Liquid.class, liquidId);
+		return liquid;
+	}
 }
