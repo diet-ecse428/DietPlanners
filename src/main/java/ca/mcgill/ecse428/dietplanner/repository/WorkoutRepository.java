6382dc615548de97f28eb29b98a117d7ff75e720
lbelli2 <lucasbellido98@hotmail.com>
2019-03-19 15:41:43 -0400

    added imports

--- src/main/java/ca/mcgill/ecse428/dietplanner/repository/WorkoutRepository.java
+++ src/main/java/ca/mcgill/ecse428/dietplanner/repository/WorkoutRepository.java
@@ -1,9 +1,11 @@
 package ca.mcgill.ecse428.dietplanner.repository;
 
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
